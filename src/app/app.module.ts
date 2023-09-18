ed1a14fe1bed42a4f19de08816f9a0530a3f357a
Jake Westerfield <jecobwesterfield@gmail.com>
2023-09-17 21:34:51 -0400

    adds in item-delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { ItemAddComponent } from './item-add/item-add.component';
 import { ItemEditComponent } from './item-edit/item-edit.component';
+import { ItemDeleteComponent } from './item-delete/item-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemListComponent,
     ItemAddComponent,
-    ItemEditComponent
+    ItemEditComponent,
+    ItemDeleteComponent
   ],
   imports: [
     BrowserModule
