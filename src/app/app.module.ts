e737b45e9e3eab1012033b9bfb282747af10d395
Jake Westerfield <jecobwesterfield@gmail.com>
2023-09-17 21:33:48 -0400

    adds in item-add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
+import { ItemAddComponent } from './item-add/item-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent
+    ItemListComponent,
+    ItemAddComponent
   ],
   imports: [
     BrowserModule
